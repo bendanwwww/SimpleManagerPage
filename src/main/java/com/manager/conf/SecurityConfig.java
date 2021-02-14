d22833c335dc70c233b9128457c486d4fb17c9b8
bendanwwww <461806307@qq.com>
2021-02-14 19:12:09 +0800

    登陆页面

--- src/main/java/com/manager/conf/SecurityConfig.java
+++ src/main/java/com/manager/conf/SecurityConfig.java
@@ -59,6 +59,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         // 设置拦截忽略文件夹，可以对静态资源放行
-        web.ignoring().antMatchers("/static/sdk", "/static/img");
+        web.ignoring().antMatchers("/sdk/**", "/img/**");
     }
 }
